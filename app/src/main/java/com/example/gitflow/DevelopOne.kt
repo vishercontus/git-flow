a3d7bb66524398ca68c089338d7d9b8bf509fed5
Vishvanath Eshwer <vishvanatheshwer.v@contus.in>
2019-04-08 16:02:21 +0530

    Changed in Develop One
    Changed in Develop Two
    
    Changed in MainActivity

--- app/src/main/java/com/example/gitflow/DevelopOne.kt
+++ app/src/main/java/com/example/gitflow/DevelopOne.kt
@@ -2,11 +2,14 @@ package com.example.gitflow
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class DevelopOne : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_develop_one)
+
+        Log.d("DevelopOne", "About")
     }
 }
--- app/src/main/java/com/example/gitflow/DevelopTwo.kt
+++ app/src/main/java/com/example/gitflow/DevelopTwo.kt
@@ -2,11 +2,14 @@ package com.example.gitflow
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class DevelopTwo : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_develop_two)
+
+        Log.d("DevelopOne", "About")
     }
 }
