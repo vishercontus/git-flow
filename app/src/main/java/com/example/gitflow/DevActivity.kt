e7648ec3f94ad1b3e939ba9f76529b6b5b46d319
Vishvanath Eshwer <vishvanatheshwer.v@contus.in>
2019-04-08 14:55:29 +0530

    Login and Dev

---
+++ app/src/main/java/com/example/gitflow/DevActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gitflow
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DevActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dev)
+    }
+}
