7ddb6331d239f719b391d52aad4401bc12f44fae
Vishvanath Eshwer <vishvanatheshwer.v@contus.in>
2019-04-08 15:23:59 +0530

    Toast Implemented

--- app/src/main/java/com/example/gitflow/FeatureOne.kt
+++ app/src/main/java/com/example/gitflow/FeatureOne.kt
@@ -2,11 +2,13 @@ package com.example.gitflow
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class FeatureOne : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feature_one)
+        Toast.makeText(this, "Feature One", Toast.LENGTH_SHORT).show()
     }
 }
