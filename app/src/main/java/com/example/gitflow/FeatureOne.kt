359cd676c235aecf95b5b8560d39a13d5b5f3f1e
Vishvanath Eshwer <vishvanatheshwer.v@contus.in>
2019-04-08 15:27:16 +0530

    Button added and click listener implemented

--- app/src/main/java/com/example/gitflow/FeatureOne.kt
+++ app/src/main/java/com/example/gitflow/FeatureOne.kt
@@ -2,13 +2,22 @@ package com.example.gitflow
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_feature_one.*
 
 class FeatureOne : AppCompatActivity() {
+
+    companion object {
+        val TAG = this.javaClass.name.toString().toUpperCase()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feature_one)
         Toast.makeText(this, "Feature One", Toast.LENGTH_SHORT).show()
+        foButton.setOnClickListener {
+            Log.d(TAG,"Button Clicked")
+        }
     }
 }
